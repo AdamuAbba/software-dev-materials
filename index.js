a4000dbf34989c5a6d75b02d8ebfd62ce5299247
shy.X <abbaadamu302@gmail.com>
2023-08-03 11:56:03 +0100

    version 1

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hi, i should run");
+console.log("hi, i should also run");
+console.log("hi, i should not run");
+
+let a = "abba";
+a = "amazing";
+a = 2;
+const b = "abba is awesome";
